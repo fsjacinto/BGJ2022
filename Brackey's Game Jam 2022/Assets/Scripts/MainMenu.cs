c34382387c659594f46fe15ac1560cba2c6819c9
kayemaceda <60454868+kayemaceda@users.noreply.github.com>
2022-08-28 02:22:06 +0800

    Settings, Audio, Font

--- Brackey's Game Jam 2022/Assets/Scripts/MainMenu.cs
+++ Brackey's Game Jam 2022/Assets/Scripts/MainMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void OnPlayButtonClicked()
     {
-
+        
     }
 
     public void QuitGame()
